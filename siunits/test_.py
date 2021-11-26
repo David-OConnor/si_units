1b4a2d4d9d93f4a24da6e8cc8a18f3cb942faa66
Christian Clauss <cclauss@me.com>
2021-11-26 07:58:34 +0100

    test_.py: import .src as u

--- siunits/test_.py
+++ siunits/test_.py
@@ -1,6 +1,6 @@
 # For use with [pytest](https://docs.pytest.org/)
 
-import src as u
+import .src as u
 
 # Note that the equality checks here included text descriptions,
 # but equality for DerivedUnit only checks the units.
